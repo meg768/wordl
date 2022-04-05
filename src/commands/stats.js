67d038691a94c4371b9c687e02ae502a59a493d0
meg768 <meg768@users.noreply.github.com>
2022-04-05 12:00:49 +0200

    -

---
+++ src/commands/stats.js
@@ -0,0 +1,48 @@
+const LetterStatistics = require('../scripts/letter-statistics.js');
+const Command = require('../scripts/command.js');
+
+module.exports = class extends Command {
+
+    constructor(options) {
+
+        super({command: 'stats [options]', description: 'Displays letter statistics', ...options}); 
+
+
+	}
+
+    options(yargs) {
+        super.options(yargs);
+    }
+
+
+	async run() {
+
+
+
+		let stats = new LetterStatistics();
+
+		this.log(`Vanligast förekommande bokstäver i Wordl är i ordning:`);
+		this.log(`${stats.frequency.alphabet}.`);
+
+		this.log('');
+
+		this.log(`Vanligaste bokstäverna i respektive ruta är följande:`);
+
+		for (let i = 0; i < 5; i++) {
+			this.log(`${stats.frequency.position[i].alphabet}`);
+		}
+		
+		this.log();
+
+		this.log(`Ord som börjar och slutar med konsonant: ${stats.match(/^[BCDFGHJKLMNPQRSTVWXZ].*[BCDFGHJKLMNPQRSTVWXZ]$/g)}%`);
+		this.log(`Ord som börjar och slutar med vokal: ${stats.match(/^[EYUIOA].*[EYUIOA]$/g)}%`);
+		this.log(`Ord med varannan konsonant/vokal: ${stats.match(/^[BCDFGHJKLMNPQRSTVWXZ][EYUIOA][BCDFGHJKLMNPQRSTVWXZ][EYUIOA][BCDFGHJKLMNPQRSTVWXZ]$/g)}%`);
+		this.log(`Ord med varannan vokal/konsonant: ${stats.match(/^[EYUIOA][BCDFGHJKLMNPQRSTVWXZ][EYUIOA][BCDFGHJKLMNPQRSTVWXZ][EYUIOA]$/g)}%`);
+		this.log(`Ord som innehåller två vokaler i följd: ${stats.match(/[EYUIOA][EYUIOA]/g)}%`);
+
+	}
+
+};
+
+
+
